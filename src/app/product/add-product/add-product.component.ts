2a241c2412641198c29f6217d2b47280bea2e3ef
miabosheva <boshevam@gmail.com>
2024-01-26 22:08:30 +0100

    reactive forms pt.1

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductType } from '../product-list/ProductType';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { ProductConditionType } from '../product-list/ProductConditionType';
 import { IProduct } from '../../model/iproduct';
@@ -12,12 +12,33 @@ import { IProduct } from '../../model/iproduct';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-  @ViewChild('Form') addProductForm: NgForm | undefined;
+[x: string]: any;
+  // @ViewChild('Form') addProductForm: NgForm | undefined;
   @ViewChild('staticTabs', { static: false }) staticTabs?: TabsetComponent;
   
   public ProductType = ProductType;
 
-  constructor(private router: Router) { }
+  addProductForm!: FormGroup;
+  nextCLicked: boolean = false;
+
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  createAddPropertyForm(){
+    this.addProductForm = this.fb.group({
+      BasicInfo: this.fb.group({
+        Title: [null, Validators.required],
+        Type: [null, Validators.required],
+        Size: [null, Validators.required],
+        Condition: [null, Validators.required],
+        Year: [null],
+        City: [null],
+        Description: [null]
+      }),
+      PriceAndPaymentInfo: this.fb.group({
+        Price: [null, Validators.required]
+      })
+    })
+  }
 
   productView: IProduct = {
     Id: -1,
@@ -33,6 +54,7 @@ export class AddProductComponent implements OnInit {
   };
 
   ngOnInit() {
+    this.createAddPropertyForm();
   }
 
   onBack(){
@@ -44,11 +66,20 @@ export class AddProductComponent implements OnInit {
     return keys.slice(keys.length / 2);
   }
 
+  getBasicInfo(){
+    return this.addProductForm.controls['BasicInfo'] as FormGroup;
+  }
+
+  getPriceAndPaymentInfo(){
+    return this.addProductForm.controls['PriceAndPaymentInfo'] as FormGroup;
+  }
+
   onSubmit(){
     console.log(this.addProductForm)
   }
 
   selectTab(tabId: number){
+    this.nextCLicked = true;
     if (this.staticTabs?.tabs[tabId]) {
       this.staticTabs.tabs[tabId].active = true;
       console.log("success");
